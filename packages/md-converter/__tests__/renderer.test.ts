39b7733eace2845ab028994ff40ade8d686180ee
yeshu <yeshu.lrt@alibaba-inc.com>
2023-05-21 10:59:33 +0800

    feat: migrate from jest to vitest

--- packages/md-converter/__tests__/renderer.test.ts
+++ packages/md-converter/__tests__/renderer.test.ts
@@ -1,3 +1,4 @@
+import { describe, test, expect } from "vitest";
 import { marked } from "marked";
 import { BaseRenderer } from "../renderer/BaseRenderer";
 import { WXRenderer } from "../renderer/WXRenderer";
